c678bf47cb615853939bca13cc6da3161fcdf9df
Bruno Lima <ibrunoliimas@gmail.com>
2025-10-25 20:35:54 -0300

    feat: create OMDB API service module with details caching

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -18,7 +18,6 @@ class Api {
         searchTerm
       )}`;
       const response = await fetch(URL);
-      
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -26,7 +25,6 @@ class Api {
 
       const data = await response.json();
       return data;
-
     } catch (error) {
       console.error("Erro ao procurar filmes e séries:", error);
 
@@ -34,6 +32,35 @@ class Api {
         Response: "False",
         Error: "Erro de rede. Tente novamente mais tarde.",
       };
+    }
+  }
+
+  async getMovieDetails(imdbID) {
+    if(this.detailsCache.has(imdbID)){
+      return this.detailsCache.get(imdbID)
+    }
+
+
+    try {
+      const url = `${API_BASE_URL}?apikey=${API_KEY}&i=${imdbID}`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new error(`HTTP error: status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      if(data.Response === 'True'){
+        this.detailsCache.set(imdbID, data);
+        return data;
+      }
+
+      return null;
+    } catch (error) {
+      console.error("Erro ao obter detalhes do filme:", error);
+      return null;
     }
   }
 }
--- src/js/api/test.js
+++ src/js/api/test.js
@@ -4,9 +4,11 @@ async function executarTeste() {
   console.log("Buscando filmes para teste...");
 
   const resultados = await testAPI.searchMovies("louca");
+  const detatlhes = await testAPI.getMovieDetails("tt0192968");
 
   console.log("EBAAAA! Resultados recebidos:");
   console.log(resultados);
+  console.log(detatlhes);
 }
 
 executarTeste();
